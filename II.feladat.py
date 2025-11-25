14cab26340dc71f7662829f9fd7ba619575422f6
IvánLevente <ivan.levente@diak.szbi-pg.hu>
2025-11-25 21:03:05 +0100

    Update II.feladat.py

--- II.feladat.py
+++ II.feladat.py
@@ -44,7 +44,50 @@ try:
         if szam < 0 and szam % 1 == 0:
             neg_int = szam
             neg_int_index = szamok.index(neg_int) + 1
-            print(f"Az első ilyen szám: {neg_int_index}")
-except ValueError:
-    print("Nem volt ilyen szám.")
+            print(f"Az első negatív szám helye: {neg_int_index}")
+            break
+except ValueError as e:
+    print(f"{e} \nNem volt ilyen szám.")
 print()
+
+#3.Hány darab 1 és 2 közé eső szám szerepel a listában:
+print("3. Hány darab 1 és 2 közé eső szám szerepel a listában?")
+
+try:
+    count = 0
+    for szam in szamok:
+        if 1 < szam < 2:
+            count += 1
+    print(f"Az 1 és 2 közé eső számok darabszáma: {count}")
+except ValueError as e:
+    print(f"{e} \nNem volt ilyen szám.")
+print()
+
+#4.Melyik volt a legnagyobb értékű negatív szám, és hányadik helyen állt:
+print("4. Melyik volt a legnagyobb értékű negatív szám, és hányadik helyen állt?")
+
+minuses = [szam for szam in szamok if szam < 0]
+
+try:
+    legnagy_minus = min(minuses)
+    min_minus = szamok.index(legnagy_minus) + 1
+    print(f"A legnagyobb ilyen értékű szám a(z) {legnagy_minus}, ami a(z) {min_minus}. helyen áll.")
+except ValueError as e:
+    print(f"{e} Nem volt ilyen szám.")
+print()
+
+#5.Mennyi a pozitív számok összege:
+print("5. Mennyi a pozitív számok összege?")
+
+poz_count = 0
+
+try:
+    for szam in szamok:
+        if szam > 0:
+            poz_count += 1
+            print(f"A pozitív számok összege: {poz_count}")
+except ValueError as e:
+    print(f"{e} \nNem volt pozitív szám")
+print(f"Összegük: {poz_count}")
+
+#VÉGE
